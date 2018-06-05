4243e58e3fb7777967fa04a08b130e6c12b87795
Sergei Grin <grinzzly@gmail.com>
2018-06-05 17:57:00 +0300

    Initial Upload

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-PacMan
+# Pacman via the WebcamController
+
+ Did you know that the original name for Pac-Man was Puck-Man? You'd think it was because he looks like a hockey puck
+ but it actually comes from the Japanese phrase 'Paku-Paku,' which means to flap one's mouth open and closed.
---
+++ src/modules/pm-controllers/datasetController.js
@@ -0,0 +1,38 @@
+import * as tf from '@tensorflow/tfjs';
+
+export class DatasetController {
+  constructor(numClasses) {
+    this.numClasses = numClasses;
+  }
+
+  /**
+   * Adds an example to the dataset controller.
+   * @param {Tensor} example.
+   * @param {number} label.
+   */
+  addExample(example, label) {
+    /* One-hot encode the label. */
+    const y = tf.tidy(
+      () => tf.oneHot(tf.tensor1d([label]).toInt(), this.numClasses));
+
+    if (this.xs == null) {
+      /* For the first example that gets added, keep example and y so that the
+      * DatasetController owns the memory of the inputs. This makes sure that
+      * if addExample() is called in a tf.tidy(), these Tensors will not get
+      * disposed.
+      */
+      this.xs = tf.keep(example);
+      this.ys = tf.keep(y);
+    } else {
+      const oldX = this.xs;
+      this.xs = tf.keep(oldX.concat(example, 0));
+
+      const oldY = this.ys;
+      this.ys = tf.keep(oldY.concat(y, 0));
+
+      oldX.dispose();
+      oldY.dispose();
+      y.dispose();
+    }
+  }
+}
